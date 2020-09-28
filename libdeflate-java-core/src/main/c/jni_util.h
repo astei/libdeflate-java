7494f6e4f4d3343dd69a0968f3fbbe3029deb070
Andrew Steinborn <git@steinborn.me>
2020-09-28 14:39:13 -0400

    Fix clang compile error(!)

--- libdeflate-java-core/src/main/c/jni_util.h
+++ libdeflate-java-core/src/main/c/jni_util.h
@@ -1,6 +1,11 @@
 #include <jni.h>
 
+#ifndef JNIUTIL_H
+#define JNIUTIL_H
+
 #define LIBDEFLATEJAVA_PUBLIC __attribute__((visibility("default")))
 
 JNIEXPORT void JNICALL
 throwException(JNIEnv *env, const char *type, const char *msg);
+
+#endif
